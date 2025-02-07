1052139c6c84a7351b86294b844a04b0eca0cdef
dawn <mohamin@naver.com>
2025-02-07 11:32:11 +0900

    0207

--- js/mode1.js
+++ js/mode1.js
@@ -207,32 +207,35 @@
      }
    });  
 
-   // 탕전기별 소요 시간
- const ctx10 = document.getElementById('decChart');  
- const decChart = new Chart(ctx10, {
+ // 조제대별 소요 시간
+ // bottombox1-2 presChart 
+ const ctx7 = document.getElementById('presChart'); 
+ const presChart = new Chart(ctx7, {
    type: 'line',
    data: {
-     labels: ['1', '2', '3', '4', '5', '6', '7', '8', '9', '10','11', '12', '13', '14', '15', '16', '17', '18', '19', '20'],
+     labels: ['1', '2', '3', '4', '5', '6', '7', '8', '9', '10', '11', '12', '13', '14', '15'],
      datasets: [{
      label: '# of last week',
-     data: [150,150,150,150,150,150,150,150,150,150,  150,150,150,150,150,150,150,150,150,150],
+     data: [3.5, 3.5, 3.5, 3.5, 3.5, 3.5, 3.5, 3.5, 3.5, 3.5, 3.5, 3.5, 3.5, 3.5, 3.5, ],
      tension:0.4,
-     borderWidth: 2,
+     borderWidth: 5,
      borderColor:'rgba(174,174,174,0.99)',
      
-     // 기존 데이터셋 설정 
+     // 기존 데이터셋 설정
+     fill: '2',
+     backgroundColor: 'rgba(0, 123, 255, 0.5)',
 
-     pointBorderColor: '#9c9c9c',
-     pointWidth:1,
-     pointRadius: 1,
-     pointBorderWidth: 1,
-     pointBackgroundColor: 'black',
+     pointBorderColor: 'white',
+     pointWidth:5,
+     pointRadius: 5,
+     pointBorderWidth: 2,
+     pointBackgroundColor: 'rgba(174,174,174,1)',
      fill: 'start',
      backgroundColor: 'rgba(236, 236, 236, 0.1)', 
      },
      {
      label: '# this week',
-     data: [120,130,140,120,120,150,150,150,150,150,  120,130,140,120,120,150,150,150,150,150],
+     data: [2.5, 3.5, 4.5, 4.5, 2.5, 3.5, 3.5, 3.5, 4.5, 4.5, 4.5, 4.5, 3.5, 3.5, 3.5, ],
      tension:0.4,
      borderWidth: 5,
      borderColor: '#C322FB', //purple
@@ -240,7 +243,7 @@
      pointRadius: 5,
      pointBorderWidth: 2,
      pointBackgroundColor: '#C322FB', //purple
-     fill: 'start',
+     fill: '1',
      backgroundColor: 'rgba(130, 53, 250, 0.2)', 
      }]
    },
@@ -249,7 +252,7 @@
      plugins: {
              title: {
                  display: true, 
-                  
+                   
              },
              legend: {
                  display:false,
@@ -265,7 +268,7 @@
      scales: {
        x: { 
          ticks: {
-           color:'#ececec'
+           color:'#5c5c5c'
          },
          grid: {
          color: '#5c5c5c', // x축 그리드 색상 변경 
@@ -273,11 +276,10 @@
        },
        y: {
          beginAtZero: false,
-         min:100,
-         max:180,
+         max:5,
          ticks: {
-           color:'#ececec',
-           stepSize: 50 // 간격을 1로 설정
+           color:'#5c5c5c',
+           stepSize: 1 // 간격을 1로 설정
          },
          grid: {
          color: '#5c5c5c' // x축 그리드 색상 변경
--- js/mode3.js
+++ js/mode3.js
@@ -19,7 +19,7 @@
   $(".GaugeMeter6").gaugeMeter({ 
     theme: 'cyonblue',
     color: '#62E9EB',  
-  });  
+  });
 
   // leftbox
   $(".GaugeMeter7").gaugeMeter({
@@ -158,7 +158,7 @@
        borderColor: '#FF34EB', //hotpink
        tension:0.4,
        
-       pointBorderColor: 'pink',
+       pointBorderColor: 'white',
        pointRadius: 5,
        pointBorderWidth: 2,
        pointBackgroundColor: '#FF34EB' //hotpink
@@ -207,98 +207,99 @@
      }
    });  
 
-   // 탕전기별 소요 시간
- const ctx10 = document.getElementById('decChart');  
- const decChart = new Chart(ctx10, {
-   type: 'line',
-   data: {
-     labels: ['1', '2', '3', '4', '5', '6', '7', '8', '9', '10','11', '12', '13', '14', '15', '16', '17', '18', '19', '20'],
-     datasets: [{
-     label: '# of last week',
-     data: [150,150,150,150,150,150,150,150,150,150,  150,150,150,150,150,150,150,150,150,150],
-     tension:0.4,
-     borderWidth: 2,
-     borderColor:'rgba(174,174,174,0.99)',
-     
-     // 기존 데이터셋 설정 
-
-     pointBorderColor: '#9c9c9c',
-     pointWidth:1,
-     pointRadius: 1,
-     pointBorderWidth: 1,
-     pointBackgroundColor: 'black',
-     fill: 'start',
-     backgroundColor: 'rgba(236, 236, 236, 0.1)', 
-     },
-     {
-     label: '# this week',
-     data: [120,130,140,120,120,150,150,150,150,150,  120,130,140,120,120,150,150,150,150,150],
-     tension:0.4,
-     borderWidth: 5,
-     borderColor: '#C322FB', //purple
-     pointBorderColor: 'pink',
-     pointRadius: 5,
-     pointBorderWidth: 2,
-     pointBackgroundColor: '#C322FB', //purple
-     fill: 'start',
-     backgroundColor: 'rgba(130, 53, 250, 0.2)', 
-     }]
-   },
-     options: {
-     responsive: true,
-     plugins: {
-             title: {
-                 display: true, 
-                  
-             },
-             legend: {
-                 display:false,
-                 labels: {
-                     color:'red'
-                 },
-                 data: {
-                     color:'white'
-                 }
-             }
-             },
-     
-     scales: {
-       x: { 
-         ticks: {
-           color:'#ececec'
-         },
-         grid: {
-         color: '#5c5c5c', // x축 그리드 색상 변경 
-       }
-       },
-       y: {
-         beginAtZero: false,
-         min:100,
-         max:180,
-         ticks: {
-           color:'#ececec',
-           stepSize: 50 // 간격을 1로 설정
-         },
-         grid: {
-         color: '#5c5c5c' // x축 그리드 색상 변경
-       }
-       }
-     },
-     // animations: {
-     //   y: {
-     //     easing: 'easeInOutElastic',
-     //     from: (ctx) => {
-     //       if (ctx.type === 'data') {
-     //         if (ctx.mode === 'default' && !ctx.dropped) {
-     //           ctx.dropped = true;
-     //           return 0;
-     //         }
-     //       }
-     //     }
-     //   },
+   //markChart 마킹기별 소요 시간
+   const ctx6 = document.getElementById('markChart'); 
+ 
+   const markChart = new Chart(ctx6, {
+     type: 'line',
+     data: {
+       labels: ['1', '2', '3', '4', '5',],
+       datasets: [{
+       label: '# of last week',
+       data: [3.5,3.5,3.5,3.5,3.5,],
+       tension:0.4,
+       borderWidth: 2,
+       borderColor:'rgba(174,174,174,0.99)',
        
-     // }, 
-     
-   }
- }); 
+       // 기존 데이터셋 설정 
+
+       pointBorderColor: '#9c9c9c',
+       pointWidth:1,
+       pointRadius: 1,
+       pointBorderWidth: 1,
+       pointBackgroundColor: 'black',
+       fill: 'start',
+       backgroundColor: 'rgba(236, 236, 236, 0.1)', 
+       },
+       {
+       label: '# this week',
+       data: [2.5,3.5,4.5,4.5,2.5],
+       tension:0.4,
+       borderWidth: 5,
+       borderColor: '#C322FB', //purple
+       pointBorderColor: 'white',
+       pointRadius: 5,
+       pointBorderWidth: 2,
+       pointBackgroundColor: '#C322FB', //purple
+       fill: 'start',
+       backgroundColor: 'rgba(130, 53, 250, 0.2)', 
+       }]
+     },
+       options: {
+       responsive: true,
+       plugins: {
+               title: {
+                   display: true, 
+                    
+               },
+               legend: {
+                   display:false,
+                   labels: {
+                       color:'red'
+                   },
+                   data: {
+                       color:'white'
+                   }
+               }
+               },
+       
+       scales: {
+         x: { 
+           ticks: {
+             color:'#ececec'
+           },
+           grid: {
+           color: '#5c5c5c', // x축 그리드 색상 변경 
+         }
+         },
+         y: {
+           beginAtZero: false,
+           min:2,
+           max:5,
+           ticks: {
+             color:'#ececec',
+             stepSize: 1 // 간격을 1로 설정
+           },
+           grid: {
+           color: '#5c5c5c' // x축 그리드 색상 변경
+         }
+         }
+       },
+       // animations: {
+       //   y: {
+       //     easing: 'easeInOutElastic',
+       //     from: (ctx) => {
+       //       if (ctx.type === 'data') {
+       //         if (ctx.mode === 'default' && !ctx.dropped) {
+       //           ctx.dropped = true;
+       //           return 0;
+       //         }
+       //       }
+       //     }
+       //   },
+         
+       // }, 
+       
+     }
+   }); 
 });
